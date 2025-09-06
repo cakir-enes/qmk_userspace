da42dcb94e78cd94b05be81bf40f3c9f9f938bec
Enes Cakir <enescakir@unknown660c22a839ac.home>
2025-09-06 15:12:56 +0300

    fix

--- keyboards/horizon/keymaps/ardakilic_trackball/keymap.c
+++ keyboards/horizon/keymaps/ardakilic_trackball/keymap.c
@@ -197,7 +197,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MS_BTN1, KC_MS_BTN1,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT /*KC_ENT*/,
         LT(_SODA, KC_GRV), KC_F19, KC_LCTL, KC_LALT,    KC_LGUI,  KC_SPC,  KC_SCRL,    KC_MS_BTN2,    KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
     ),
-,
 
 /* Lower
  * ,-------------------------------------------------------------------------------------------------.
